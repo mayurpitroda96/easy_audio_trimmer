e1c26c48a35c98de557cb2a44b7e499d6a56a0c4
eugenz3rr <eugen.zerr@the-olymp.net>
2024-03-02 18:01:07 +0100

    refs #11 Replaced ffmpeg with the right version for audio.

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
-import 'package:path/path.dart';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_audio/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'utils/file_formats.dart';
